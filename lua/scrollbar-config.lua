6d8d818a6e255bdf2f7978c5e922158f2cffdbcc
Matthew Hooper <matthewjhooper@hotmail.com>
2025-01-10 21:09:34 +1100

    formatted and made some changes to the scrollbar config

--- lua/scrollbar-config.lua
+++ lua/scrollbar-config.lua
@@ -1,6 +1,6 @@
 require("scrollbar").setup({
     show = true,
-    show_in_active_only = false,
+    show_in_active_only = true,
     set_highlights = true,
     folds = 1000, 
     max_lines = false,
@@ -9,100 +9,62 @@ require("scrollbar").setup({
     handle = {
         text = " ",
         blend = 30,
-        color = nil,
-        color_nr = nil, -- cterm
         highlight = "CursorColumn",
-        hide_if_all_visible = true, -- Hides handle if all lines are visible
+        hide_if_all_visible = true,
     },
     marks = {
         Cursor = {
             text = "•",
             priority = 0,
-            gui = nil,
-            color = nil,
-            cterm = nil,
-            color_nr = nil, -- cterm
             highlight = "Normal",
         },
         Search = {
-            text = { "-", "│" },
+            text = { "-", "=" },
             priority = 1,
-            gui = nil,
             color = "#FF8800",
-            cterm = nil,
-            color_nr = nil, -- cterm
             highlight = "Search",
         },
         Error = {
             text = { "-", "=" },
             priority = 2,
-            gui = nil,
-            color = nil,
-            cterm = nil,
-            color_nr = nil, -- cterm
             highlight = "DiagnosticVirtualTextError",
         },
         Warn = {
             text = { "-", "=" },
             priority = 3,
-            gui = nil,
-            color = nil,
-            cterm = nil,
-            color_nr = nil, -- cterm
             highlight = "DiagnosticVirtualTextWarn",
         },
         Info = {
             text = { "-", "=" },
             priority = 4,
-            gui = nil,
-            color = nil,
-            cterm = nil,
-            color_nr = nil, -- cterm
             highlight = "DiagnosticVirtualTextInfo",
         },
         Hint = {
             text = { "-", "=" },
             priority = 5,
-            gui = nil,
-            color = nil,
-            cterm = nil,
-            color_nr = nil, -- cterm
             highlight = "DiagnosticVirtualTextHint",
         },
         Misc = {
             text = { "-", "=" },
             priority = 6,
-            gui = nil,
-            color = nil,
-            cterm = nil,
-            color_nr = nil, -- cterm
             highlight = "Normal",
         },
         GitAdd = {
             text = "█",
             priority = 7,
-            gui = nil,
             color = "#98be65",
-            cterm = nil,
-            color_nr = nil, -- cterm
             highlight = "GitSignsAdd",
         },
         GitChange = {
             text = "█",
             priority = 7,
-            gui = nil,
             color = "#98be65",
-            cterm = nil,
-            color_nr = nil, -- cterm
             highlight = "GitSignsChange",
         },
         GitDelete = {
             text = "█",
             priority = 7,
-            gui = nil,
             color = "#ec5f67",
-            cterm = nil,
-            color_nr = nil, -- cterm
             highlight = "GitSignsDelete",
         },
     },
@@ -137,9 +99,9 @@ require("scrollbar").setup({
     handlers = {
         cursor = true,
         diagnostic = true,
-        gitsigns = true, -- Requires gitsigns
+        gitsigns = true,
         handle = true,
-        search = true, -- Requires hlslens
-        ale = false, -- Requires ALE
+        search = true,
+        ale = false,
     },
 })
